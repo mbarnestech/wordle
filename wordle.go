946dc5513f26b77515ae1eeb34bc7b122d5e1e6a
mbarnestech <maggie.barnes@gmail.com>
2023-05-16 10:49:15 -0600

    work through implementing newLetter and newGuess

--- wordle.go
+++ wordle.go
@@ -1,1 +1,75 @@
 package wordle
+
+const (
+	maxGuesses = 6
+	wordSize   = 5
+)
+
+// letterStatus can be none, correct, present, or absent
+type letterStatus int
+
+const (
+	// none = no status, not guessed yet
+	none letterStatus = iota // 0
+	// absent = not in the word
+	absent // 1
+	// present = in the word, but not in the correct position
+	present // 2
+	// correct = in the correct position
+	correct // 3
+)
+
+type wordleState struct {
+	// word is the word that the user is trying to guess
+	word [wordSize]byte
+	// guesses holds the guesses that the user has made
+	guesses [maxGuesses]guess
+	// currGuess is the index of the available slot in guesses
+	currGuess int
+}
+
+// guess is an attempt to guess the word
+type guess [wordSize]letter
+
+type letter struct {
+	// char is the letter that this struct represents
+	char byte
+	// status is the state of the letter (absent, present, correct)
+	status letterStatus
+}
+
+// newWordleState builds a new wordleState from a string.
+// Pass in the word you want the user to guess.
+func newWordleState(word string) wordleState {
+
+	w := wordleState{
+		word:      [wordSize]byte([]byte(word)),
+		guesses:   [maxGuesses]guess{},
+		currGuess: 0,
+	}
+
+	// copy(w.word[:], word) // takes string word, copies it into array of bytes
+
+	return w
+}
+
+// newLetter should take in a character (as a byte) and return
+// a new letter with that character and a none status.
+
+func newLetter(char byte) letter {
+	return letter{
+		char:   char,
+		status: none,
+	}
+}
+
+// newGuess should take in a string and return a new guess.
+// You should loop over each letter in the string and convert them to letter structs.
+
+func newGuess(s string) guess {
+	var g guess
+	for i, char := range s {
+		g[i] = newLetter(byte(char))
+	}
+	return g
+}
--- wordle_test.go
+++ wordle_test.go
@@ -1,1 +1,33 @@
 package wordle
+
+import "testing"
+
+func TestNewWordleState(t *testing.T) {
+	word := "HELLO"
+	ws := newWordleState(word)
+	wordleAsString := string(ws.word[:])
+
+	t.Logf("	Created wordleState:")
+	t.Logf("    word: %s", wordleAsString)
+	t.Logf("    guesses: %v", ws.guesses)
+	t.Logf("    currGuess: %v", ws.currGuess)
+
+	if wordleAsString != "HELLO" {
+		t.Errorf("Expected word to be %s but got %s", word, wordleAsString)
+	}
+}
+
+func statusToString(status letterStatus) string {
+	switch status {
+	case none:
+		return "none"
+	case correct:
+		return "correct"
+	case present:
+		return "present"
+	case absent:
+		return "absent"
+	default:
+		return "unknown"
+	}
+}
